0f603173cb1ab3a941cbf3b81d30ad58a375899a
Ashley Medina <ashley-medina@lambdastudents.com>
2020-05-19 07:20:47 -0600

    Finished Sprint

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,4 +1,4 @@
-struct Flavor {
+struct Flavors {
     var name: String
     var rate: Double
     
@@ -17,33 +17,65 @@ let large = Size.large
 
 
 struct Cone {
-    let coneflavor: Flavor
-    let topping: String
-    let size: Size
+    let coneFlavor: Flavors
+    var topping: String
+    var size: Size
     
-  
-    }
-   
-    func eat() {
-        print("Mmmm! I love \(Cone.coneflavor.Flavor).")
+   func eat() {
+    print("Mmmm! I love \(coneFlavor.name)!")
+ }
 }
-
-
 
 class IceCreamShop {
     
-    var flavorNames: [Flavor]
+    var flavorNames: Flavors
     var toppings: String
-    var size: Size
+    var size: [Size]
     var totalSales: Double
    
-    init(flavorNames: [Flavor], toppings: [String], size: [Size]){
-        self.flavorNames = Flavor
+    init(flavorNames: Flavors, toppings: String, size: [Size], totalSales: Double) {
+        self.flavorNames = flavorNames
         self.toppings = toppings
-        self.sizes = Sizes
+        self.size = size
         self.totalSales = 0.0
+    }
+    func listTopFlavors() {
+        for flavor in flavor.rate {
+            if flavors.rate > 4.0 {
+                print("Our top flavors are \(flavor.name).")
+            }
+        }
+    }
+
+    func orderCone(flavor: Flavors, topping: String?, size: Size) -> Cone? {
+        
+        let newCone = Cone(coneFlavor: flavor, topping: toppings, size: size)
+        totalSales += size.rawValue
+        if let orderCone = newCone.topping {
+            print("Your \(newCone.coneFlavor.name) with ice cream is \(size.rawValue)")
+        }else{
+            print("Your \(newCone.coneFlavor.name) ice cream is \(size.rawValue)")
+        }
+        return newCone
     }
 }
 
+let strawberry = Flavors(name: "strawberry", rate: 10.0)
+let vanilla = Flavors(name: "vanilla", rate: 6.0)
+let chocolate = Flavors(name: "chocolate", rate: 3.0)
+let flavArray = [strawberry, vanilla, chocolate]
 
 
+let topsArray = ["chocolateSauce", "sprinkles", "nuts"]
+let szArray = [Size.small, Size.medium, Size.large]
+
+let myIceCreamShop = IceCreamShop(flavorNames: flavArray, toppings: "topsArray", size: szArray, totalSales: 0)
+
+myIceCreamShop.listTopFlavors()
+print(myIceCreamShop.totalSales)
+
+let orderedCone = myIceCreamShop.orderCone(flavor: strawberry, topping: topsArray[1], size: .medium)
+print(orderedCone)
+orderedCone?.eat
+
+print(myIceCreamShop.totalSales)
